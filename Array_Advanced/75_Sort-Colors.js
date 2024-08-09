54c7af7cbbd700e0ed06835ace5badb8213c868f
Vinayak Navghane <navghanevinayak24@gmail.com>
2024-08-10 01:34:22 +0530

    Sort colors

---
+++ Array_Advanced/75_Sort-Colors.js
@@ -0,0 +1,49 @@
+/*
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.
+
+Example 1:
+
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+Example 2:
+
+Input: nums = [2,0,1]
+Output: [0,1,2]
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 300
+nums[i] is either 0, 1, or 2.
+*/
+
+/*
+1. We can solve this problem by using quick sort , but that would be an overkill for this problem.
+
+2. For better solution we will use Dutch National flag algorithm.
+*/
+
+var sortColors = function (nums) {
+    let low = 0, mid = 0, high = nums.length - 1;
+
+    while (mid <= high) {
+        if (nums[mid] === 0) {
+            [nums[low], nums[mid]] = [nums[mid], nums[low]];
+            low++;
+            mid++;
+        } else if (nums[mid] === 1) {
+            mid++;
+        } else {
+            [nums[mid], nums[high]] = [nums[high], nums[mid]];
+            high--;
+        }
+    }
+};
+
+// TC - O(n)
+// SC - O(1)
