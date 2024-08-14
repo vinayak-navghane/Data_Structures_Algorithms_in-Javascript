19b3c3e3ff0a95fa013cfe8103ded487eff08434
Vinayak Navghane <navghanevinayak24@gmail.com>
2024-08-14 17:40:33 +0530

    Search in Rotated Sorted Array

--- Binary_Search/33_SearchInRotatedSortedArray.js
+++ Binary_Search/33_SearchInRotatedSortedArray.js
@@ -47,7 +47,7 @@ var search = function (nums, target) {
         return nums[0] === target ? 0 : -1;
     }
 
-    for (let i = 0; i < nums.length - 1; i++) {  // Adjusted loop boundary
+    for (let i = 0; i < nums.length - 1; i++) {
         if (nums[i] > nums[i + 1]) {
             pivot = i + 1;
             break;
@@ -70,6 +70,9 @@ var search = function (nums, target) {
 
 // Optimized -
 
+// [4,5,6,7,0,1,2]
+//  l     m     r
+
 var search = function (nums, target) {
     let left = 0;
     let right = nums.length - 1;
@@ -80,23 +83,23 @@ var search = function (nums, target) {
         if (nums[mid] === target)
             return mid;
 
-        else if (nums[mid] >= nums[left]) {
-            if (nums[left] <= target && target <= nums[mid]) {
+        if (nums[left] <= nums[mid]) {
+
+            if (nums[left] <= target && target < nums[mid]) {
                 right = mid - 1;
-            } else {
-                left = mid + 1;
             }
+            else left = mid + 1;
         }
         else {
             if (nums[mid] <= target && target <= nums[right]) {
                 left = mid + 1;
-            } else {
-                right = mid - 1;
             }
+            else right = mid - 1;
         }
     }
-
     return -1;
 };
 
-// TC - O(nlogn)
+// TC - O(logn)
+
+
