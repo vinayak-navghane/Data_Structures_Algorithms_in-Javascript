a2c91e87dc57fe4c46bdf4e560a1fd4f788c34fd
Vinayak Navghane <navghanevinayak24@gmail.com>
2024-08-30 23:58:34 +0530

    Insert at given Position

--- LinkedList/linkedlist.js
+++ LinkedList/linkedlist.js
@@ -17,3 +17,64 @@ class LinkedList {
         this.head = null
     }
 }
+
+//Insert at Beginning
+
+// LinkedList.prototype.insertAtBeginning = function (data) {
+//     const newNode = new Node(data);
+//     this.head = newNode;
+// }
+
+/* Insert at the End - we need to check where next is pointing to null, then 
+   insert node there(at last)
+*/
+
+// LinkedList.prototype.insertAtEnd = function (data) {
+//     const newNode = new Node(data);
+//     if (!this.head) {
+//         this.head = newNode;
+//         return;
+//     }
+
+//     let last = this.head;
+//     while (last.next) {
+//         last = last.next;
+//     }
+
+//     last.next = newNode;
+
+// }
+
+
+
+LinkedList.prototype.insertAtBeginning = function (data) {
+    const newNode = new Node(data);
+    this.head = newNode;
+}
+
+
+LinkedList.prototype.insertAtEnd = function (data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+        this.head = newNode;
+        return;
+    }
+
+    let last = this.head;
+    while (last.next) {
+        last = last.next;
+    }
+    last.next = newNode;
+}
+
+//Insert at given Position
+
+LinkedList.prototype.insertAfter = function (prevNode, data) {
+    if (!prevNode) {
+        console.log("Given prev node cannot be null");
+        return;
+    }
+
+    const newNode = new Node(data, prevNode.next);
+    prevNode.next = newNode;
+}
