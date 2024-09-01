a80d2d9e6eee48381125c9e6c1ad11bd69b0d51f
Vinayak Navghane <navghanevinayak24@gmail.com>
2024-09-02 00:45:50 +0530

    reverse LL using recursion

--- LinkedList/reverseLinkedList.js
+++ LinkedList/reverseLinkedList.js
@@ -33,3 +33,40 @@ class Solution {
 
 
 //TC - O(n)
+
+
+// Using recursion - 
+
+/*
+class Node{
+    constructor(data){
+        this.data = data;
+        this.next = null;
+    }
+}
+*/
+
+class Solution {
+    // Function to reverse a linked list.
+    reverseList(head) {
+        // Base case: if the list is empty or has only one node, return head
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        // Recursively reverse the rest of the linked list
+        let newHead = this.reverseList(head.next);
+
+        // After recursion, head.next is the last node in the reversed sublist
+        // Make it point back to the current head
+        head.next.next = head;
+
+        // Set the next of the current head to null to avoid cycles
+        head.next = null;
+
+        // Return the new head of the reversed linked list
+        return newHead;
+    }
+}
+
+// TC - O(n)
