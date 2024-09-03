5ab35897636859e2d15408ea9767d1d9ab4c99d0
Vinayak Navghane <navghanevinayak24@gmail.com>
2024-09-04 01:26:54 +0530

    Intersection of Two Linked Lists

--- LinkedList/160_IntersectionOfTwoLinkedLists.js
+++ LinkedList/160_IntersectionOfTwoLinkedLists.js
@@ -4,7 +4,6 @@ Given the heads of two singly linked-lists headA and headB, return the node at w
 */
 //Brute force - 
 
-
 var getIntersectionNode = function (headA, headB) {
     let mySet = new Set()
 
@@ -26,3 +25,25 @@ var getIntersectionNode = function (headA, headB) {
 
 // TC - O(n + m)
 // SC - O(n)
+
+//Optimized - 
+
+
+var getIntersectionNode = function (headA, headB) {
+    let t1 = headA
+    let t2 = headB
+
+    while (t1 != t2) {
+        t1 = t1.next
+        t2 = t2.next
+
+        if (t1 == t2) return t1
+
+        if (t1 == null) t1 = headB
+        if (t2 == null) t2 = headA
+    }
+    return t1
+}
+
+//TC - O(n1 + n2)
+//SC - O(1)
