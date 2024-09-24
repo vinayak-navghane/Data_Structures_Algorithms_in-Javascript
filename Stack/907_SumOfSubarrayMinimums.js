b1c5ba57d6dbd747bd054d0913670905802e94ac
Vinayak Navghane <navghanevinayak24@gmail.com>
2024-09-25 00:13:15 +0530

    Sum of Subarray Minimums

--- Stack/907_SumOfSubarrayMinimums.js
+++ Stack/907_SumOfSubarrayMinimums.js
@@ -16,43 +16,55 @@ Input: arr = [11,81,94,43,3]
 Output: 444
 */
 
+/*
+Intuition:
+
+Monotonic Stack: Use stacks to efficiently find the nearest smaller elements to the left and right for each array element.
+
+previous smallest equal element and next smallest element: Track distances to the nearest smaller elements to determine the range where each element is the minimum.
+
+Contribution Calculation: For each element, calculate its contribution to the sum of subarray minimums by multiplying its value with the count of subarrays where it is the minimum.
+
+This approach ensures that each element's contribution is counted in constant time, resulting in an overall linear time complexity.
+*/
+
 
 var sumSubarrayMins = function (arr) {
-    let n = arr.length;
-    let left = new Array(n).fill(0); //O(n)
-    let right = new Array(n).fill(0); //O(n)
-    let stack = [];
+    let n = arr.length
+    let nse = new Array(n).fill(0)
+    let psee = new Array(n).fill(0)
+    let stack = []
 
-    // Calculate left - for loop takes O(n) & while loop can also  
-    // takes maximum O(n) so TC - O(2n)
+    //calculating previous smallest equal element
+    //for loop takes O(n) & while loop can also takes maximum O(n) 
+    //so TC - O(2n)
     for (let i = 0; i < n; i++) {
-        while (stack.length && arr[stack[stack.length - 1]] > arr[i]) {
-            stack.pop();
+        while (stack.length && arr[stack[stack.length - 1]] > arr[i]) { // do not use = sign here
+            stack.pop()
         }
-        left[i] = stack.length ? stack[stack.length - 1] : -1;
-        stack.push(i);
+        psee[i] = stack.length ? stack[stack.length - 1] : -1
+        stack.push(i)
     }
 
-    stack = [];
+    stack = []
 
-    // Calculate right - for loop takes O(n) & while loop can also  
-    // takes maximum O(n) so TC - O(2n)
+    //calculating next smallest element
+    //for loop takes O(n) & while loop can also takes maximum O(n) 
+    //so TC - O(2n)
     for (let i = n - 1; i >= 0; i--) {
-        while (stack.length && arr[stack[stack.length - 1]] >= arr[i]) {
-            stack.pop();
+        while (stack.length && arr[stack[stack.length - 1]] >= arr[i]) { // use = sign here
+            stack.pop()
         }
-        right[i] = stack.length ? stack[stack.length - 1] : n;
-        stack.push(i);
+        nse[i] = stack.length ? stack[stack.length - 1] : n
+        stack.push(i)
     }
 
-    // Calculate the sum of minimums of all subarrays
     let sum = 0;
-    for (let i = 0; i < n; i++) { //O(n)
-        sum += arr[i] * (i - left[i]) * (right[i] - i);
-        sum %= 1e9 + 7; // To handle large numbers, use modulo
+    for (let i = 0; i < n; i++) {
+        sum += arr[i] * (nse[i] - i) * (i - psee[i])
+        sum %= 1e9 + 7
     }
-
-    return sum;
+    return sum
 };
 
 /*
